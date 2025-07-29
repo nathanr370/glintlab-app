92a95724a15b49c38aad565ff39188d0cefce152
nathanr37 <nathan_rhee@berkeley.edu>
2025-07-28 17:35:50 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'GlintApp - Beta',
+  description: 'Data Visualization for Glint Lab - created by Nathan Rhee',
 }
 
 export default function RootLayout({
